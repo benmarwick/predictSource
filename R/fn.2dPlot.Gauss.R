41d012995737baade2a77c34d824c29ed26f9222
John Karon <sierrastew@mindspring.com>
2018-08-12 21:13:24 -0600

    Improve fn.2dPlot.Gauss() code and documentation, to show QQ plots appropriately.

--- R/fn.2dPlot.Gauss.R
+++ R/fn.2dPlot.Gauss.R
@@ -1,6 +1,6 @@
 #' fn.2dPlot.Gauss
 #'
-#' Plot and check specified pairs of analysis variables for bivariate normality. The function stops after producing each plot.  Enter c ("continue") at the prompt to get the next plot.  If this function is run using Rstudio, each plot appears in a separate window, not in the Rstudio plot pane.
+#' Plot and check specified pairs of analysis variables for bivariate normality. The function stops after producing each plot.  Enter c ("continue") at the prompt to get the next plot.
 #'
 #' @param doc  A string documenting use written to the output list; default is the function name
 #' @param data  R matrix or data frame containing the data to be analyzed
@@ -9,22 +9,22 @@
 #' @param Groups  Vector of values of the group variable for which plots are to be done;
 #'    if "All", use all groups; if " ", no grouping
 #' @param AnalyticVars  Names of two analytic variables to be shown in the plots, vector of length 2
-#' @param QQtest  Logical (default is T): specify whether to run the package qqtest
+#' @param qqPlot  Logical (default is T): specify whether to show q-q plots, including plots based on simulation and multivariate plots
 #' @param pvalue.digits  Numeric (default is 3): number of significant digits retained in tests for normality
-#' @param Identify  Logical(default is F): if T, user can identify points of interest in the plots
+#' @param Identify  Logical(default is F): if T, user can identify points of interest in the plots; qqPlot must be T
 #' @param folder  In Windows, path to the folder containing an excel file with the identified points, ending with two forward slashes;
 #'         if " ", no data set is written
 #' @param ds.identified  Excel file name with extension .csv containing information on the identified points
 #'
 #' @return   A list with the following components:
 #'  \itemize{
-#' \item{usage}{  Vector with the contents of the argument doc, the date run, the version of R used}
-#' \item{dataUsed}{ The contents of the argument data restricted to the groups used}
-#' \item{analyticVars}{  The contents of the argument AnalyticVars}
-#' \item{parameters}{  A vector with argument values for GroupVar, Groups, pvalue.digits, and QQtest}
-#' \item{pvalues}{  A matrix with the p-values for univariate and bivariate tests of normality}
-#' \item{data.check}{ If Identify = T, a data frame with the information on user-identified points of interest}
-#' \item{file}{ If folder != " ", a string or list: the path to the excel file or files with pvalues and, if Identify = T,
+#' \item{usage:}{  Vector with the contents of the argument doc, the date run, the version of R used}
+#' \item{dataUsed:}{ The contents of the argument data restricted to the groups used}
+#' \item{analyticVars:}{  The contents of the argument AnalyticVars}
+#' \item{parameters:}{  A vector with argument values for GroupVar, Groups, pvalue.digits, and QQtest}
+#' \item{pvalues:}{  A data frame with the p-values for univariate and bivariate tests of normality}
+#' \item{data.check:}{ If Identify = T, a data frame with the information on user-identified points of interest}
+#' \item{file:}{ If folder != " ", a string or list: the path to the excel file or files with pvalues and, if Identify = T,
 #'  the information on user-identified points of interest}
 #' }
 #'
@@ -41,7 +41,7 @@
 #' @export
 #'
 fn.2dPlot.Gauss <- function (doc = "fn.2dPlot.Gauss", data, GroupVar,labID, Groups,
-          AnalyticVars, QQtest = T, pvalue.digits=3, Identify=F, folder=" ", ds.pvalues, ds.data.check)
+          AnalyticVars, qqPlot = T, pvalue.digits=3, Identify=F, folder=" ", ds.pvalues, ds.data.check)
 {
   if (length(AnalyticVars)!=2)  stop("length of AnalyticVars must be 2")
   #
@@ -67,26 +67,30 @@ fn.2dPlot.Gauss <- function (doc = "fn.2dPlot.Gauss", data, GroupVar,labID, Grou
   fn.plot <- function() {
     temp <- data.Used[data.Used[, GroupVar] == groups[i.group],AnalyticVars]
     temp1 <- temp[, AnalyticVars[1]]
-    if (QQtest) qqtest(data = temp1, dist = "normal", drawPercentiles = T,
+    if (qqPlot) {
+      qqnorm.pts<-qqnorm(temp1, main = paste(AnalyticVars[1],"source", groups[i.group]))
+      qqline(temp1)
+      qqtest(data = temp1, dist = "normal", drawPercentiles = T,
                        main = paste(AnalyticVars[1],"source", groups[i.group]))
-    qqnorm.pts<-qqnorm(temp1, main = paste(AnalyticVars[1],"source", groups[i.group]))
-    qqline(temp1)
+      }
     if (Identify) {
       index<-identify(qqnorm.pts)
       data.grp<-data.Used[data.Used[,GroupVar]==groups[i.group],]
       data.check<<-rbind(data.check,data.grp[index,])
-    }
+      }
     temp2 <- temp[, AnalyticVars[2]]
-    if (QQtest) qqtest(data = temp2, dist = "normal", drawPercentiles = T,
+    if (qqPlot) {
+      qqnorm.pts<-qqnorm(temp2, main = paste(AnalyticVars[2],"source", groups[i.group]))
+      qqline(temp2)
+      qqtest(data = temp2, dist = "normal", drawPercentiles = T,
            main = paste(AnalyticVars[2],"source", groups[i.group]))
-    qqnorm.pts<-qqnorm(temp2, main = paste(AnalyticVars[2],"source", groups[i.group]))
-    qqline(temp2)
+      browser()
+      }
     if (Identify) {
       index<-identify(qqnorm.pts)
       data.grp<-data.Used[data.Used[,GroupVar]==groups[i.group],]
       data.check<<-rbind(data.check,data.grp[index,])
     }
-    browser()
     ADp1 <- ad.test(temp1)$p.value
     ADp2 <- ad.test(temp2)$p.value
     SWp1 <- shapiro.test(temp1)$p.value
@@ -94,9 +98,13 @@ fn.2dPlot.Gauss <- function (doc = "fn.2dPlot.Gauss", data, GroupVar,labID, Grou
     mardia <- MVN::mvn(data = temp, mvnTest="mardia")
     p.mardia.skew <- as.numeric(as.character(mardia[[1]][[3]][[1]], mode="numeric"))
     p.mardia.kurtosis <- as.numeric(as.character(mardia[[1]][[3]][[2]], mode="numeric"))
-    hz <- MVN::mvn(data=temp, mvnTest="hz",multivariatePlot="qq")
+    if (qqPlot) {
+      hz <- MVN::mvn(data=temp, mvnTest="hz",multivariatePlot="qq")
+      browser()
+      }
+      else  hz <- MVN::mvn(data=temp, mvnTest="hz")
     p.hz <- as.numeric(hz[[1]][[3]], mode = "numeric")
-    royston <- MVN::mvn(data=temp, mvnTest="royston",multivariatePlot="qq")
+    royston <- MVN::mvn(data=temp, mvnTest="royston")
     p.royston <- as.numeric(royston[[1]][[3]], mode = "numeric")
     p.temp <- c(ADp1, ADp2, SWp1, SWp2, p.mardia.skew, p.mardia.kurtosis, p.hz, p.royston)  # return p-values
     p.temp
@@ -104,8 +112,14 @@ fn.2dPlot.Gauss <- function (doc = "fn.2dPlot.Gauss", data, GroupVar,labID, Grou
   #
   pvalues <- matrix(NA, nrow=length(groups), ncol = 8)
   par(mfrow = c(2, 2))
-  for (i.group in 1:length(groups))
+  for (i.group in 1:length(groups)) {
+    if ((i.group > 1) & (qqPlot==T)) {
+      plot.new()
+      plot.new()
+      plot.new() # three blank plots, so next group starts in new window
+      }
     pvalues[i.group, ] <- fn.plot()
+    }
   #
   numeric.pvalues<-as.numeric(pvalues)
   numeric.pvalues<-round(numeric.pvalues,dig=pvalue.digits)
@@ -131,7 +145,7 @@ fn.2dPlot.Gauss <- function (doc = "fn.2dPlot.Gauss", data, GroupVar,labID, Grou
   }
   #
   fcn.date.ver<-c(doc,date(),R.Version()$version.string)
-  parameters<-c(groupVar=GroupVar,groups=Groups,digits.pvalue=pvalue.digits,qqtest=QQtest)
+  parameters<-c(groupVar=GroupVar,groups=Groups,digits.pvalue=pvalue.digits,qqPlot=qqPlot, Identify=Identify)
   if (substr(folder,1,1) == " ") {
     if ((!Identify)) out<-list(usage=fcn.date.ver,dataUsed=data.Used,analyticVars=AnalyticVars,parameters=parameters,
                                pvalues=return.pvalues)
