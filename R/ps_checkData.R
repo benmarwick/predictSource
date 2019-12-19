dfa04e7ef29dcd00908f658e6a25a05ec165c249
JohnKaron <34497877+JohnKaron@users.noreply.github.com>
2019-12-19 09:43:07 -0700

    Corrected ps_checkData(): added argument ByGroup, rounding of descriptive statistics to 2 decimals if all values <10, corresponding changes to documentation.  Corresponding correction to vignette.

--- R/ps_checkData.R
+++ R/ps_checkData.R
@@ -6,11 +6,10 @@
 #' @param doc A character string written to the output list; default is the function name
 #' @param data  An R object (data frame) containing analytic data
 #' @param CheckDupVars  A vector with names of identifying variables, typically group and lab ID
-#' @param GroupVar  If there are groups, name of variable defining the groups,
-#'  default value of " ", no grouping
+#' @param GroupVar  The name of variable defining the groups (required)
 #' @param Groups  A character vector of groups by which numbers of samples and statistics will be
-#'  tabulated.  Default is " ", tabulations are done for the entire data set.
-#'  If value ls "All", tabulation for each distinct code in GroupVar
+#'  tabulated or "All"
+#' @param ByGroup  Logical: default is TRUE. If FALSE, tabulations are for all groups combined
 #' @param ID  The name of lab ID, default is " " (no lab ID)
 #' @param AnalyticVars  A character vector of names of analytic variables for which tabulations are done
 #' @param folder  The path to the folder in which data frames will be saved; default is " ", no path
@@ -25,17 +24,19 @@
 #' \item{NegativeValues:}{  A data frame containing the observations with at least one negative
 #'  value for a variable in AnalyticVars}
 #' \item{Nvalues:}{  A data frame contain the number of observations with a value for each analytic variable}
-#' \item{statistics:}{  A data frame containing the statistics statistics (by group, if Group is specified)}
+#' \item{statistics:}{  A data frame containing the descriptive statistics (by group, if ByGroup = TRUE)}
 #' \item{location:}{  The value of the parameter folder}
 #' }
 #'
 #' @section Detail:
 #' AnalyticVars must be a vector of length at least 2.  If Groups specifies selected groups (is
-#' not equal to "All" or " "), it must be a vector of length at least 2.  The function returns a list
+#' not equal to "All"), it must be a vector of length at least 2.  The function returns a list
 #' with four data frames: duplicate observations, observations with negative
-#'       values for one or more analytic variables, numbers of observations for each analytic
-#'       variable, and statistics statistics (quartiles and number missing).
-#'       If Groups != " ", numbers of observations and statistics statistics are by group.
+#' values for one or more analytic variables, numbers of observations for each analytic
+#' variable, and descriptive statistics (quantiles and number missing).
+#' If the largest values is < 10 (true if use log10 transforms), the descriptive statistics
+#' are rounded to 2 digits, otherwise to integers.
+#' If ByGroup=TRUE, numbers of observations and statistics statistics are by group.
 #'
 #' @import  assertthat
 #'
@@ -43,7 +44,7 @@
 #' data(ObsidianSources)
 #' analyticVars<-c("Rb","Sr","Y","Zr","Nb")
 #' dataCheck<-ps_checkData(data=ObsidianSources,CheckDupVars=analyticVars,GroupVar="Code",Groups="All",
-#' ID = "ID", AnalyticVars=analyticVars)
+#' ByGroup=TRUE, ID = "ID", AnalyticVars=analyticVars)
 #'
 #' @export
 
@@ -51,8 +52,9 @@ ps_checkData <-
   function(doc = "pc_checkData",
            data,
            CheckDupVars,
-           GroupVar = " ",
-           Groups,
+           GroupVar,
+           Groups = "All",
+           ByGroup = TRUE,
            ID = " ",
            AnalyticVars,
            folder = " ") {
@@ -64,6 +66,7 @@ ps_checkData <-
                 msg="parameter CheckDupVars not a vector of names")
     assert_that(is.character(GroupVar), msg="parameter GroupVar not character")
     assert_that(is.character(Groups), msg="parameter Groups not character")
+    assert_that(is.logical(ByGroup), msg="parameter ByGroup not logical")
     assert_that(is.vector(AnalyticVars)&is.character(AnalyticVars),
                 msg="parameter AnalyticVars not a character vector")
     assert_that(is.character(ID), msg="parameter ID not character valued")
@@ -71,7 +74,7 @@ ps_checkData <-
     #
     #  restrict data if specified
     #
-    if ((Groups[1] != " ") & (Groups[1] != "All")) {
+    if (Groups[1] != "All") {
       UseRows <- (data[, GroupVar] %in% Groups)
       dataUsed <- data[UseRows, ]
     }
@@ -79,11 +82,9 @@ ps_checkData <-
     #
     #  sort on GroupVar and ID if specified
     #
-    if (GroupVar[1] != " ") {
       rowsSort <- order(dataUsed[,GroupVar])
       dataUsed <- dataUsed[rowsSort,]
-    }
-    if (ID[1] != " ") {
+     if (ID[1] != " ") {
       rowsSort <- order(dataUsed[,ID])
       dataUsed <- dataUsed[rowsSort,]
     }
@@ -103,14 +104,15 @@ ps_checkData <-
     if (sum(NegRows) == 0)
       NegativeValues <- NA
     else    NegativeValues <- data[NegRows, ]
- #
-      if ((GroupVar[1] == " ") | (Groups[1] == " ")) {
+    #
+      if (ByGroup == FALSE) {
       Nvalues <- rep(0, length(AnalyticVars))
       for (i in 1:length(AnalyticVars))
         Nvalues[i] <- sum(!is.na(data[, AnalyticVars[i]]))
+      names(Nvalues)<-AnalyticVars
       }
     #
-    if (Groups[1] == "All") {
+    if ((ByGroup == TRUE) & (Groups[1] == "All")) {
       groups <- as.character(unique(dataUsed[, GroupVar]))
       n_groups <- length(groups)
       nvalues <- matrix(0, nrow = n_groups + 1, ncol = length(AnalyticVars))
@@ -126,7 +128,7 @@ ps_checkData <-
       Nvalues <- data.frame(Group = c(groups, "Total"), nvalues)
     } # end of code for Groups == "All"
     #
-    if ((Groups[1] != " ") & (Groups[1] != "All")) {
+    if ((ByGroup == TRUE) & (Groups[1] != "All")) {
       n_groups <- length(Groups)
       nvalues <- matrix(0, nrow = n_groups + 1, ncol = length(AnalyticVars))
       for (i in 1:n_groups) {
@@ -140,19 +142,19 @@ ps_checkData <-
       colnames(nvalues) <- AnalyticVars
       Nvalues <- data.frame(Group = c(Groups, "Total"), nvalues)
     } # end of loop for restricted groups
-#
-    if (GroupVar == " ") {
+    #
+    #  descriptive statistics
+    #
+    if (ByGroup == FALSE) {
       statistics <- matrix(NA, nrow = length(AnalyticVars), ncol = 7)
       for (j in 1:length(AnalyticVars)) {
         statisticsj <- summary(dataUsed[, AnalyticVars[j]])
         statistics[j, 1:length(statisticsj)] <- statisticsj
       } # end of loop on j
-      statistics_values <- round(statistics, digits = 0)
-      colnames(statistics_values) <- c("min", "Q1", "median",
-                                       "mean", "Q3", "max", "n_missing")
-      statistics_values[is.na(statistics_values[, 7]), 7] <- 0
-     } # end of code for GroupVar == " "
-    else if (Groups[1] != " ") {
+      statistics_values <- statistics
+      }
+      # end of code for ByGroup = FALSE
+    else if (ByGroup == TRUE) {
       if (Groups[1] == "All")  groups <- as.character(unique(dataUsed[, GroupVar]))
         else  groups <- Groups
       n_groups <- length(groups)
@@ -173,15 +175,22 @@ ps_checkData <-
         row <- row + n_groups + 1
       }  # end of loop on i
       }  # end of code for specified groups
-      statistics_values <- round(statistics_values, digits = 0)
+      #
+      #  round descriptive statistics to 2 digits if all values < 10, else round to integers
+      #
+      if (max(statistics_values[,-7],na.rm=TRUE) < 10)
+        statistics_values[,-7]<-round(statistics_values[,-7],digits = 2)
+      else  statistics_values[,-7]<-round(statistics_values[,-7],digits = 0)
       colnames(statistics_values) <- c("min", "Q1", "median",
                                     "mean", "Q3", "max", "n_missing")
+      #  if no missing values, replace NA by 0
       statistics_values[is.na(statistics_values[, 7]), 7] <- 0
-      if (GroupVar == " ")
+      #
+      if (ByGroup == FALSE)
         statistics <- data.frame(Analysis = AnalyticVars, statistics_values)
       else  statistics <- data.frame(Analysis = vector_values, Group = vector_groups,
                             statistics_values)
-      #
+     #
     fcnDateVersion<-c(doc,date(),R.Version()$version.string)
     params<-list(CheckDupVars,GroupVar,Groups)
     names(params)<-c("CheckDupVars","GroupVar","Groups")
